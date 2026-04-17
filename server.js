2e59f9b4959c0c3096454f1977038f7896ee9583
ValkiriaSex <Yinethbueno2618@gmail.com>
2026-04-16 20:11:31 -0500

    fix error render require

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import path from "path";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
@@ -17,7 +17,7 @@ let productos = [];
 // RUTA PRINCIPAL
 // =====================
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // =====================
